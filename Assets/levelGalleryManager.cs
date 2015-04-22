eabd0e49fc274e8dadd5191eb52e65e86ab72097
Mattias <mattias.linder87@gmail.com>
2015-04-22 11:21:26 +0200

    level select pictures

---
+++ Assets/levelGalleryManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class levelGalleryManager : MonoBehaviour
+{
+	public Image currentImg;
+	public Sprite[] mySprites;
+	private int currentLevel;
+
+	void Start () {
+		currentLevel = 0;
+		currentImg.sprite = mySprites [currentLevel];
+	}
+
+	public void changeLevel () {
+		if (currentLevel + 1 >= mySprites.Length) {
+			currentLevel = 0;
+		} else {
+			currentLevel++;
+		}
+		currentImg.sprite = mySprites [currentLevel];
+	}
+
+	public void saveLevel() {
+		PlayerPrefs.SetInt ("level",currentLevel);
+	}
+
+}
