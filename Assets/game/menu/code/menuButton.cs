fd46944db3187659b0956127ef795ac36697a394
Mattias <mattias.linder87@gmail.com>
2015-04-22 10:29:51 +0200

    better and bigger menu system

---
+++ Assets/game/menu/code/menuButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void test() {
+		Debug.Log ("Quit application");
+		Animator a = GetComponent<Animator> ();
+		a.SetBool ("Open",false);
+		Application.Quit ();
+	}
+}
