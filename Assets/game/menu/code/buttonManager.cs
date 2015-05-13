82bc03bf5b0067cecc30cb254121ac408689c6c1
Mattias <mattias.linder87@gmail.com>
2015-05-13 13:06:49 +0200

    can not change controls

--- Assets/game/menu/code/buttonManager.cs
+++ Assets/game/menu/code/buttonManager.cs
@@ -11,6 +11,40 @@ public class buttonManager : MonoBehaviour {
 	public Toggle regenerateLife;
 	public translation translationTool;
 
+	// Player one inputs
+	public InputField jump;
+	public InputField shoot;
+	public InputField left;
+	public InputField right;
+	public InputField up;
+	public InputField down;
+
+	// Player two inputs
+	public InputField jump2;
+	public InputField shoot2;
+	public InputField left2;
+	public InputField right2;
+	public InputField up2;
+	public InputField down2;
+
+	public void Awake() {
+		PlayerPrefs.SetString ("jump","space");
+		PlayerPrefs.SetString ("shoot","z");
+		PlayerPrefs.SetString ("right","d");
+		PlayerPrefs.SetString ("left","a");
+		PlayerPrefs.SetString ("up","w");
+		PlayerPrefs.SetString ("down","s");
+		PlayerPrefs.SetString ("next","e");
+		PlayerPrefs.SetString ("prev","q");
+
+		PlayerPrefs.SetString ("jump2","m");
+		PlayerPrefs.SetString ("shoot2","n");
+		PlayerPrefs.SetString ("right2","ö");
+		PlayerPrefs.SetString ("left2","k");
+		PlayerPrefs.SetString ("up2","o");
+		PlayerPrefs.SetString ("down2","l");
+	}
+	
 	public void back() {
 		Debug.Log ("Go back");
 	}
@@ -59,5 +93,65 @@ public class buttonManager : MonoBehaviour {
 	public void OnGUI() {
 		bloodScrollBar ();
 		livesScrollBar ();
+	}
+
+	public void checkForSpace(string button, InputField field) {
+		if (field.text == " ") {
+			field.text = "space";
+			PlayerPrefs.SetString(button, field.text);
+		}
+	}
+
+	public void setControllButton(string button, InputField field) {
+		PlayerPrefs.SetString(button,field.text);
+		Debug.Log(button +": "+ PlayerPrefs.GetString(button));
+		checkForSpace(button, field);
+
+		Debug.Log ("A: " + KeyCode.A);
+	}
+	
+	public void setControl(string button) {
+		Debug.Log ("set: " + button);
+		switch (button) {
+			case "right":
+				setControllButton(button, jump);	
+				break;
+			case "jump":
+				setControllButton(button, jump);
+				break;
+			case "shoot":
+				setControllButton(button, shoot);
+				break;
+			case "left":
+				setControllButton(button, left);
+				break;
+			case "up":
+				setControllButton(button, up);
+				break;
+			case "down":
+				setControllButton(button, down);
+				break;
+			case "right2":
+				setControllButton(button, right2);
+				break;
+			case "jump2":
+				setControllButton(button, jump2);
+				break;
+			case "shoot2":
+				setControllButton(button, shoot2);
+				break;
+			case "left2":
+				setControllButton(button, left2);
+				break;
+			case "up2":
+				setControllButton(button, up2);
+				break;
+			case "down2":
+				setControllButton(button, down2);
+				break;
+			default:
+				Debug.Log("default");
+				break;
+		}
 	}
 }
