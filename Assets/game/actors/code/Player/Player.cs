4a6624ef4288d297d01e7aedd0f12feb89fe092c
Daniel-Backlund <d.backlund.85@gmail.com>
2015-05-19 15:25:12 +0200

    player och lite annat

--- Assets/game/actors/code/Player/Player.cs
+++ Assets/game/actors/code/Player/Player.cs
@@ -1,248 +1,204 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Spine;
+using System;
 
 public class Player : MonoBehaviour {
-	public bool _isFacingRight;
-	public CharacterController2D _controller;
-	private float _normalizedHorizontalSpeed;
-	private float _normalizedHorizontalSpeedP2;
+  public bool _isFacingRight;
+  public CharacterController2D _controller;
+  private float _normalizedHorizontalSpeed;
+
+  public float MaxSpeed = 8f;
+  public float SpeedAccelerationOnGround = 10f;
+  public float SpeedAccelerationInAir = 5f;
+
+  public bool justFlipped = false;
+  public Aim aim;
+
+  //Input variables player
+  private string moveRight;
+  private string moveLeft;
+  private string aimUp;
+  private string aimDown;
+  private string prevWeapon;
+  private string nextWeapon;
+  private string shoot;
+  private string jump;
+  private string digging;
+
+  //Weapon variables
+  public ArrayList weaponList = new ArrayList ();
+  public int currentWeapon = 0;
+  public ChangeWeapon changeWeapon;
+  private weapon weapon;
+
 	
-	public float MaxSpeed = 8f;
-	public float SpeedAccelerationOnGround = 10f;
-	public float SpeedAccelerationInAir = 5f;
-  
-	public bool justFlipped = false;
-	public bool player1;
-	public bool player2;
-  	public Aim aim;
+  //digging
+  private Digging dig;
 
- 	// input variables player one
-	private string moveRightPlayer1;
-	private string moveLeftPlayer1;
-	private string aimUpPlayer1;
-	private string aimDownPlayer1;
-	private string prevWeaponPlayer1;
-	private string nextWeaponPlayer1;
-	private string shootPlayer1;
-	private string jump1;
-	private string digPlayer1;
+  //camera
+  private Camera cam;
 
-	private string moveRightPlayer2;
-	private string moveLeftPlayer2;
-	private string aimUpPlayer2;
-	private string aimDownPlayer2;
-	private string shootPlayer2;
-	private string jump2;
-	private string digPlayer2;
+  //Spine animation
 
-	//Weapon variables
-	public ArrayList weaponList = new ArrayList ();
-	public int currentWeapon = 0;
-	public ChangeWeapon changeWeapon;
-	private weapon weapon;
+  [SpineAnimation("idle")]
+  public string idleAnimation;
 
-	//digging
-	private Digging dig;
+  [SpineAnimation("run")]
+  public string moveAnimation;
 
+  [SpineAnimation("attack")]
+  public string attackAnimation;
 
-	public void Start() {	
-		_controller = GetComponent<CharacterController2D>();
-    	_isFacingRight = transform.localScale.x > 0;
-		//Debug.Log("transform = " + _controller.transform);
-		aim.aiming(_isFacingRight, _controller.transform.position);
-		for (int i=0; i < 5; i++) {
-			weaponList.Add(i);
-		}
-		weapon = new weapon ();
-		dig = new Digging ();
+  [SpineAnimation("jump")]
+  public string jumpAnimation;
 
+  [SpineSlot]
+  public string gunSlot;
 
-		// The real keybindings choosen from the main menu
-		/* 
-		moveRightPlayer1 = PlayerPrefs.GetString ("right");
-		moveLeftPlayer1 = PlayerPrefs.GetString ("left");
-		aimUpPlayer1 = PlayerPrefs.GetString ("up");
-		aimDownPlayer1 = PlayerPrefs.GetString ("down");
-		prevWeaponPlayer1 = PlayerPrefs.GetString ("next");
-		nextWeaponPlayer1 = PlayerPrefs.GetString ("prev");
-		shootPlayer1 = PlayerPrefs.GetString ("shoot");
-		jump1 = PlayerPrefs.GetString ("jump");
-		digPlayer1 = ;
+  [SpineAttachment(currentSkinOnly: true, slotField: "gunSlot")]
+  public string carbineAttachment;
 
-		moveRightPlayer2 = PlayerPrefs.GetString ("right2");
-		moveLeftPlayer2 = PlayerPrefs.GetString ("left2");
-		aimUpPlayer2 = PlayerPrefs.GetString ("up2");
-		aimDownPlayer2 = PlayerPrefs.GetString ("down2");
-		shootPlayer2 = PlayerPrefs.GetString ("shoot2");
-		jump2 = PlayerPrefs.GetString ("jump2");
-		digPlayer2 =;
-		*/
+  [SpineAttachment(currentSkinOnly: true, slotField: "gunSlot")]
+  public string mp40Attachment;
 
-		// Temporary keybindings for developing purposes
-		moveRightPlayer1 = "d";
-		moveLeftPlayer1 = "a";
-		aimUpPlayer1 = "w";
-		aimDownPlayer1 = "s";
-		prevWeaponPlayer1 = "e";
-		nextWeaponPlayer1 = "1";
-		shootPlayer1 = "z";
-		jump1 = "space";
-		digPlayer1 = "f";
+  private SkeletonAnimation skeletonAnimation = null;
 
-		moveRightPlayer2 = "right";
-		moveLeftPlayer2 = "left";
-		aimUpPlayer2 = "up";
-		aimDownPlayer2 = "down";
-		shootPlayer2 = "m";
-		jump2 = "right shift";
-		digPlayer2 = "n";
+  public void Setup(String moveLeft, String moveRight, String aimUp, String aimDown, String prevWeapon, String nextWeapon, String shoot, String jump, String digging, int positionX, int positionY) {
+    this.moveRight = moveRight;
+    this.moveLeft = moveLeft;
+    this.aimUp = aimUp;
+    this.aimDown = aimDown;
+    this.prevWeapon = prevWeapon;
+    this.nextWeapon = nextWeapon;
+    this.shoot = shoot;
+    this.jump = jump;
+	this.digging = digging;
+    transform.position = new Vector2(positionX, positionY);
+  }
 
+  public void Start() {
+    // Get the SkeletonAnimation component for the GameObject this script is attached to.
+    skeletonAnimation = GetComponent<SkeletonAnimation>();
+
+    skeletonAnimation.skeleton.SetAttachment(gunSlot, carbineAttachment);
+
+    _controller = GetComponent<CharacterController2D>();
+    _isFacingRight = transform.localScale.x > 0;
+    //Debug.Log("transform = " + _controller.transform);
+    aim.aiming(_isFacingRight, _controller.transform.position);
+    for (int i=0; i < 5; i++) {
+      weaponList.Add(i);
+    }
+    weapon = new weapon ();
+	dig = new Digging  ();
+	cam = GetComponent<Camera> ();
+  }
+
+  public void Update() {
+    HandleInput();
+    var movementFactor = _controller.State.IsGrounded ? SpeedAccelerationOnGround : SpeedAccelerationInAir;
+      _controller.SetHorizontalForce (Mathf.Lerp (_controller.Velocity.x, _normalizedHorizontalSpeed * MaxSpeed, Time.deltaTime * movementFactor));
+      aim.aiming (_isFacingRight, _controller.transform.position);
+  }
+
+  private void HandleInput() {
+
+    //MOVE RIGHT
+    if (Input.GetKey(moveRight)) {
+
+      _normalizedHorizontalSpeed = 1;
+      if(!_isFacingRight) {
+        Flip();
+        aim.flipAim(_isFacingRight);
+      }
+      if (_controller.State.IsGrounded){
+        skeletonAnimation.AnimationName = moveAnimation;
+      }
+
+    //MOVE LEFT
+    } else if (Input.GetKey(moveLeft)) {
+      _normalizedHorizontalSpeed = -1;
+      if(_isFacingRight) {
+        Flip();
+        aim.flipAim(_isFacingRight);
+      }
+      if (_controller.State.IsGrounded) {
+        skeletonAnimation.AnimationName = moveAnimation;
+      }
+    //IDLE
+    } else {
+      _normalizedHorizontalSpeed = 0;
+      if (_controller.State.IsGrounded) {
+        skeletonAnimation.AnimationName = idleAnimation;
+      }
+
+    }
+    if (_controller.CanJump && Input.GetKeyDown(jump)) {
+      skeletonAnimation.AnimationName = jumpAnimation;
+      _controller.Jump();
+    }
+    //Aimingkey input
+    if (Input.GetKeyDown(aimUp)) {
+      aim.up = true;
+    }
+
+    if (Input.GetKeyUp (aimUp)) {
+      aim.up = false;
+    }
+    if (Input.GetKeyDown(aimDown)) {
+      aim.down = true;
+    }
+
+    if (Input.GetKeyUp (aimDown)) {
+      aim.down = false;
+    }
+	
+	if (Input.GetKeyDown (digging)) {
+		dig.Dig(_controller.transform.position, aim.angle, aim.sight.position, _controller.transform.rotation, _isFacingRight);
 	}
 
+    //Weaponchange input
+    if (Input.GetKeyDown(prevWeapon)) {
+      if (currentWeapon == 0) {
+        currentWeapon = changeWeapon.weaponList.Count-1;
+      } else {
+        currentWeapon -= 1;
+      }
+      changeWeapon.swapWeapon(currentWeapon);
+    }
 
-	public void Update() {
-    	HandleInput();
-    	var movementFactor = _controller.State.IsGrounded ? SpeedAccelerationOnGround : SpeedAccelerationInAir;
-		if (player1) {
-			_controller.SetHorizontalForce (Mathf.Lerp (_controller.Velocity.x, _normalizedHorizontalSpeed * MaxSpeed, Time.deltaTime * movementFactor));
-			aim.aiming (_isFacingRight, _controller.transform.position);
-		} else {
-			_controller.SetHorizontalForce (Mathf.Lerp (_controller.Velocity.x, _normalizedHorizontalSpeedP2 * MaxSpeed, Time.deltaTime * movementFactor));
-			aim.aiming (_isFacingRight, _controller.transform.position);
-		}
-	}
+    if (Input.GetKeyDown(nextWeapon)) {
+      if (currentWeapon == changeWeapon.weaponList.Count-1) {
+        currentWeapon = 0;
+      } else {
+        currentWeapon += 1;
+      }
+      changeWeapon.swapWeapon(currentWeapon);
+    }
 
-	private void HandleInput() {
+    if (Input.GetKeyDown (shoot) ){
+      weapon.shoot(_controller.transform.position, aim.angle, aim.sight.position,_controller.transform.rotation,_isFacingRight);
+      //skeletonAnimation.AnimationName = attackAnimation;
+    }
+  }
 
-    	if (Input.GetKey(moveRightPlayer1) && player1) {
-    		_normalizedHorizontalSpeed = 2;
-			if(!_isFacingRight) {
-        		Flip();
-				aim.flipAim(_isFacingRight);
-      		}
-    	} else if (Input.GetKey(moveLeftPlayer1) && player1) {
-      		_normalizedHorizontalSpeed = -2;
-      		if(_isFacingRight) {
-        		Flip();	
-				aim.flipAim(_isFacingRight);
-      		}
-    	} else {
-      		_normalizedHorizontalSpeed = 0;
-    	}
-	
-		if (_controller.CanJump && Input.GetKeyDown(jump1) && player1) {
-      		_controller.Jump();
-    	}
-	
-		//Aimingkey input
-		if (Input.GetKeyDown(aimUpPlayer1) && player1) {
-				aim.up = true;
-		}
+  private void Flip() {
+    justFlipped = true;
+    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    _isFacingRight = transform.localScale.x > 0;
+    aim.arrow.localScale = new Vector3(-aim.arrow.localScale.x, aim.arrow.localScale.y, aim.arrow.localScale.z);
+    if (aim.aimingBelow && !_isFacingRight) {
+      aim.arrow.rotation = Quaternion.AngleAxis (aim.degAngle, Vector3.forward);
 
-		if (Input.GetKeyUp (aimUpPlayer1) && player1) {
-				aim.up = false;	
-		}
-		
-		if (Input.GetKeyDown(aimDownPlayer1) && player1) {
-				aim.down = true;
-		}
+    } else  if (aim.aimingBelow && _isFacingRight) {
+      aim.arrow.rotation = Quaternion.AngleAxis (-aim.degAngle, Vector3.forward);
 
-		if (Input.GetKeyUp (aimDownPlayer1) && player1) {
-				aim.down = false;
-		}
+    } else if (_isFacingRight) {
+      aim.arrow.rotation = Quaternion.AngleAxis (aim.degAngle, Vector3.forward);
+    } else {
+      aim.arrow.rotation = Quaternion.AngleAxis (-aim.degAngle, Vector3.forward);
+    }
 
-		//Weaponchange input
-		if (Input.GetKeyDown(prevWeaponPlayer1) && player1) {
-			if (currentWeapon == 0) {
-				currentWeapon = changeWeapon.weaponList.Count-1;
-			} else {
-			    currentWeapon -= 1;
-			}
-				changeWeapon.swapWeapon(currentWeapon);
-		}
-
-		if (Input.GetKeyDown(nextWeaponPlayer1) && player1) {
-			if (currentWeapon == changeWeapon.weaponList.Count-1) {
-				currentWeapon = 0;
-			} else {
-				currentWeapon += 1;
-			}
-				changeWeapon.swapWeapon(currentWeapon);
-		}
-
-		if (Input.GetKeyDown (shootPlayer1) && player1 ) {
-			weapon.shoot(_controller.transform.position, aim.angle, aim.sight.position,_controller.transform.rotation,_isFacingRight);
-
-	
-		}
-
-		if (Input.GetKeyDown (digPlayer1) && player1 ) {
-			dig.Dig(_controller.transform.position, aim.angle, aim.sight.position, _controller.transform.rotation, _isFacingRight);
-		}
-
-		//player2 handeling
-		if (Input.GetKey(moveRightPlayer2) && player2) {
-			_normalizedHorizontalSpeedP2 = 2;
-			if(!_isFacingRight) {
-				Flip();
-				aim.flipAim(_isFacingRight);
-			}
-		} else if (Input.GetKey(moveLeftPlayer2) && player2) {
-			_normalizedHorizontalSpeedP2 = -2;
-			if(_isFacingRight) {
-				Flip();	
-				aim.flipAim(_isFacingRight);
-			}
-		} else {
-			_normalizedHorizontalSpeedP2 = 0;
-		}
-
-			//Aimingkey input
-		if (Input.GetKeyDown(aimUpPlayer2) && player2) {
-			aim.up = true;
-		}
-		
-		if (Input.GetKeyUp (aimUpPlayer2) && player2) {
-			aim.up = false;	
-		}
-		
-		if (Input.GetKeyDown(aimDownPlayer2) && player2) {
-			aim.down = true;
-		}
-			
-		if (Input.GetKeyUp (aimDownPlayer2) && player2) {
-			aim.down = false;
-		}
-
-		if (Input.GetKeyDown (shootPlayer2) && player2 ) {
-			weapon.shoot(_controller.transform.position, aim.angle, aim.sight.position,_controller.transform.rotation,_isFacingRight);
-		}
-
-		if (_controller.CanJump && Input.GetKeyDown(jump2) && player2) {
-			_controller.Jump();
-		}
-
-		if (Input.GetKeyDown (digPlayer2) && player2 ) {
-			dig.Dig(_controller.transform.position, aim.angle, aim.sight.position, _controller.transform.rotation, _isFacingRight);
-		}
-	}
-
-	private void Flip() {
-		justFlipped = true;
-	    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-	    _isFacingRight = transform.localScale.x > 0;
-		aim.arrow.localScale = new Vector3(-aim.arrow.localScale.x, aim.arrow.localScale.y, aim.arrow.localScale.z);
-		if (aim.aimingBelow && !_isFacingRight) {
-			aim.arrow.rotation = Quaternion.AngleAxis (aim.degAngle, Vector3.forward);
-
-		} else  if (aim.aimingBelow && _isFacingRight) {
-			aim.arrow.rotation = Quaternion.AngleAxis (-aim.degAngle, Vector3.forward);
-
-		} else if (_isFacingRight) {
-			aim.arrow.rotation = Quaternion.AngleAxis (aim.degAngle, Vector3.forward);
-		} else {
-			aim.arrow.rotation = Quaternion.AngleAxis (-aim.degAngle, Vector3.forward);
-		}
-	
-	}
+  }
 }
